e55c741d33bd191a118074a25d9131324804687d
DashaZubareva <znacomca@inbox.ru>
2017-11-24 17:34:52 +0200

    ModelBike add biuld

--- BikeStore/BikeStore/Models/Bike.cs
+++ BikeStore/BikeStore/Models/Bike.cs
@@ -1,4 +1,5 @@
 ﻿using BikeStore.Enums;
+using BikeStore.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +8,30 @@ using System.Web;
 namespace BikeStore.Models
 {
     public class Bike
-    {
+    {       
         public int BikeId { get; set; }
         public string BikeName { get; set; }
-        public EnumCategory BikeCategory{ get; set; }
+        public string BikeCategory{ get; set; }
         public int BikeWheel { get; set; }
         public int BikeSpeed { get; set; }
         public decimal BikePrice { get; set; }
 
-        public IList<Bike> build()
+        private static Random random = new Random();
+        public Bike build()
         {
-            IList<Bike> bikes = new IList<Bike>();
-            return 
+            int index = random.Next(1, 100);
+            int speedVal = random.Next(10, 30);
+            int wheellVal = random.Next(3, 14);
+            double PriceVal = random.Next(500, 5000);
+
+            return new Bike
+            {
+                BikeId = index,
+                BikeName = UtilForBike.ModelsBike[random.Next(0, 12)],
+                BikeCategory = EnumCategory.1;
+
+
+            }
         }
 
     }
