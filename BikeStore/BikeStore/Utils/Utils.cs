5970bc2fa240bb43c8cad0f7f773c777e78d4efb
DashaZubareva <znacomca@inbox.ru>
2017-11-28 17:00:42 +0200

    BikeFromWork

---
+++ BikeStore/BikeStore/Utils/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeStore.Utils
+{
+    public class Utils
+    {
+        public static int GetInt(int? val)
+        {
+            int x = 0;
+            int.TryParse("" + val, out x);
+            return x;
+        }
+    }
+}
